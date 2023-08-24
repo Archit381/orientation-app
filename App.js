296c1cef23202d6ac0a5e0e367441d7b0e32ff6b
Archit <architojha420@gmail.com>
2023-08-24 14:41:06 +0530

    built the apk

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './navigation/appNavigation';
+import { LogBox } from "react-native"
+
+LogBox.ignoreAllLogs()
+
 
 
 export default function App() {
